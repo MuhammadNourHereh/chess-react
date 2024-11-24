d17d08326ae3238211789763d3c1fef854e0cc84
Muhammad Nour <acadamiclearner@gmail.com>
2024-11-24 20:14:29 +0200

    creating the board

---
+++ src/component/Board.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Square from './Square'
+import './Board.css'
+
+const Board = () => {
+  return (
+    <div className='board'>
+      <Square />
+    </div>
+  )
+}
+
+export default Board
---
+++ src/component/Square.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import './Square.css'
+
+const Square = () => {
+
+  const squares = <>
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+    <div className='square light' />
+    <div className='square dark' />
+  </>
+
+
+  return (
+    squares
+  )
+}
+
+export default Square
